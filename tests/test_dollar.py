75adf48ef4872b870d19f7d0d0b691f5241e1062
koseki <kosekitau@gmail.com>
2026-01-05 22:38:32 +0900

    RED timesの連続利用時のテストに失敗

--- tests/test_dollar.py
+++ tests/test_dollar.py
@@ -1,8 +1,11 @@
+import pytest
 from src.dollar import Dollar
 
 
-class Test_Monkey:
+class Test_Money:
     def test_Multiplication(self) -> None:
         five = Dollar(amount=5)
         five.times(multiplier=2)
         assert 10 == five.amount
+        five.times(multiplier=3)
+        assert 15 == five.amount

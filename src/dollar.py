34e1c6c7d97a06b07a6a0f76df9dea9917976b3d
koseki <kosekitau@gmail.com>
2026-01-05 17:02:20 +0900

    Refactoring

--- src/dollar.py
+++ src/dollar.py
@@ -1,7 +1,7 @@
 class Dollar:
     def __init__(self, amount: int) -> None:
-        self.amount: int = 10
+        self.amount: int = amount
         pass
 
-    def times(self, multiplier: int):
-        pass
+    def times(self, multiplier: int) -> None:
+        self.amount *= multiplier
--- tests/test_dollar.py
+++ tests/test_dollar.py
@@ -2,7 +2,7 @@ from src.dollar import Dollar
 
 
 class Test_Monkey:
-    def test_Multiplication(self):
-        five = Dollar(5)
-        five.times(2)
+    def test_Multiplication(self) -> None:
+        five = Dollar(amount=5)
+        five.times(multiplier=2)
         assert 10 == five.amount

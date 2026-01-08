afecd3c8275555d4e7c47d5aeaa8058981183cb0
koseki <kosekitau@gmail.com>
2026-01-08 23:10:07 +0900

    test:times() でDollarオブジェクトを返すテストに失敗

--- tests/test_dollar.py
+++ tests/test_dollar.py
@@ -6,9 +6,9 @@ class Test_Money:
     def test_Multiplication(self) -> None:
         five = Dollar(amount=5)
         product: int = five.times(multiplier=2)
-        assert 10 == product
+        assert Dollar(amount=10) == product
         product = five.times(multiplier=3)
-        assert 15 == product
+        assert Dollar(amount=15) == product
 
     def test_Equality(self) -> None:
         assert Dollar(amount=5).equals(other=Dollar(amount=5))

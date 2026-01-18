8e31278eaf69a0b6117e224f09b9d9728f8c5d33
koseki <kosekitau@gmail.com>
2026-01-18 10:44:38 +0900

    feat:Francの等価性のテストを追加

--- tests/test_money.py
+++ tests/test_money.py
@@ -14,6 +14,8 @@ class Test_Money:
     def test_Equality(self) -> None:
         assert Dollar(amount=5) == Dollar(amount=5)
         assert Dollar(amount=5) != Dollar(amount=6)
+        assert Franc(amount=5) == Franc(amount=5)
+        assert Franc(amount=5) != Franc(amount=6)
 
     def test_FrancMultiplication(self) -> None:
         five = Franc(amount=5)

f031fdbdc4ea66e04fceed3ec854d522058c5443
koseki <kosekitau@gmail.com>
2026-01-18 14:02:44 +0900

    test:Money.dollarでDollarが返ってくるテストに失敗

--- tests/test_money.py
+++ tests/test_money.py
@@ -1,15 +1,14 @@
 import pytest
 from src.dollar import Dollar
 from src.franc import Franc
+from src.money import Money
 
 
 class Test_Money:
     def test_Multiplication(self) -> None:
-        five = Dollar(amount=5)
-        product: int = five.times(multiplier=2)
-        assert Dollar(amount=10) == product
-        product = five.times(multiplier=3)
-        assert Dollar(amount=15) == product
+        five = Money.dollar(amount=5)
+        assert Dollar(amount=10) == five.times(2)
+        assert Dollar(amount=15) == five.times(3)
 
     def test_Equality(self) -> None:
         assert Dollar(amount=5) == Dollar(amount=5)

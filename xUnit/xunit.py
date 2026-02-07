95b90f25bc3670f3db013d801ea6386d80c95813
koseki <kosekitau@gmail.com>
2026-02-07 10:21:45 +0900

    feat:testMethodするとwasRunが1になる

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -1,4 +1,14 @@
-test = WasRun("testMethod")
+class WasRun:
+    def __init__(self, name) -> None:
+        pass
+        self.wasRun = None
+
+    def testMethod(self) -> None:
+        pass
+        self.wasRun = 1
+
+
+test: WasRun = WasRun(name="testMethod")
 print(test.wasRun)
 test.testMethod()
 print(test.wasRun)

87784bc7ee43f449404d1e5848a572e79e22d631
koseki <kosekitau@gmail.com>
2026-02-08 11:49:15 +0900

    test:setupができてるか確認のテストに失敗

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name: str) -> None:
         self.name: str = name
 
+    def setUp(self) -> None:
+        pass
+
     def run(self) -> None:
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -15,13 +19,24 @@ class WasRun(TestCase):
     def testMethod(self) -> None:
         self.wasRun = 1
 
+    def setUp(self) -> None:
+        self.wasSetUp = 1
+
 
 class TestCaseTest(TestCase):
+    """ここがテストを先に書く場になる"""
+
     def testRunning(self) -> None:
         test: WasRun = WasRun(name="testMethod")
         assert not test.wasRun
         test.run()
         assert test.wasRun
 
+    def testSetUp(self) -> None:
+        test: WasRun = WasRun(name="testMethod")
+        test.run()
+        assert test.wasSetUp
+
 
 TestCaseTest(name="testRunning").run()
+TestCaseTest(name="testSetUp").run()
